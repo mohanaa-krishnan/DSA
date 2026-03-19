77b28f766a30a5128e8cf9e1a934f74e35b99a5f
mohanaa krishnan <mohanaakrishnan12@gmail.com>
2026-03-19 22:38:53 +0530

    linked list:insertion at specfic position using recursion

---
+++ linkedlsit/insertionByrecursion.c
@@ -0,0 +1,86 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+struct node
+{
+    int data;
+    struct node *next;
+};
+int n;
+struct node *insertion(int n);
+void display(struct node *ptr);
+struct node *reinsertion(int value,int idx,struct node *curr,int a);
+int main()
+{
+    //int n;
+    scanf("%d",&n);
+    struct node *head=insertion(n);
+    int value,idx;
+    scanf("%d",&value);
+    scanf("%d",&idx);
+    if(idx>0 && idx<=n)
+    {
+    struct node *newhead=reinsertion(value,idx,head,n);
+    
+    display(newhead);
+    }
+    else
+    {
+        display(head);
+    }
+    return 0;
+}
+void display(struct node *ptr)
+{
+    if(ptr==NULL)
+    {
+        printf("empty");
+        return;
+    }
+    struct node *temp=ptr;
+    while(temp!=NULL)
+    {
+        printf("%d",temp->data);
+        temp=temp->next;
+    }
+}
+struct node *insertion(int n)
+{
+    struct node *head=NULL;
+        struct node *temp=NULL;
+    struct node *newnode=NULL;
+    for(int i=0;i<n;i++)
+    {
+        newnode=malloc(sizeof(struct node));
+        if(newnode==NULL)
+        {
+            exit(1);
+        }
+        scanf("%d",&newnode->data);
+        newnode->next=NULL;
+        if(head==NULL)
+        {
+            head=temp=newnode;
+        }
+        else
+        {
+            temp->next=newnode;
+            temp=newnode;
+        }
+    }
+    return head;
+}
+
+struct node *reinsertion(int value,int idx,struct node *curr,int a)
+{
+
+    if(idx==1)
+    {
+        struct node *newnode=malloc(sizeof(struct node));
+        newnode->data=value;
+        newnode->next=curr;
+        return newnode;
+    }
+    curr->next=reinsertion(value,idx-1,curr->next,a);
+    return curr;
+}
